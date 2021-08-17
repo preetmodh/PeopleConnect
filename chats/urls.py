7da154d5dae4bb75755a4081a9e305578c1ffc65
preetmodh <preetmodh@gmail.com>
2021-08-17 11:59:42 +0530

    merge master

--- chats/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import ChatListCreateView,RecentChatListView
-
-urlpatterns = [
-    path('inbox/<str:chat_id>/',ChatListCreateView.as_view()),
-    path('recent/',RecentChatListView.as_view()),
-]
