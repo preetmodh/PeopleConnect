f386dfa8c274eb51617829b3bf2b16ea64a74c19
preetmodh <preetmodh@gmail.com>
2021-08-17 10:26:36 +0530

    nc

--- Django/core/settings.py
+++ Django/core/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
 }
 
 
-GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'coral-sum-321710-a4637f09d154.json'
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'Django\core\coral-sum-321710-a4637f09d154.json'
 
 
 WSGI_APPLICATION = 'core.wsgi.application'
