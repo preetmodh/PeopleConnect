695242e45b58c2d4c26088dc42841482ccbfcc02
preetmodh <preetmodh@gmail.com>
2021-08-18 11:51:22 +0530

    nc

--- core/asgi.py
+++ core/asgi.py
@@ -1,15 +1,16 @@
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from notifications.consumers import *
 from chats.consumers import *
 from django.urls import path
 from .token_auth import TokenAuthMiddlewareStack
-from channels.asgi import get_channel_layer
-
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-channel_layer = get_channel_layer()
+django.setup()
+channel_layer = get_default_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     # Just HTTP for now. (We can add other protocols later.)
