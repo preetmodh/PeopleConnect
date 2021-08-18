c8243d5de936c81aaa461163a9e3a2f4cd6c1787
preetmodh <preetmodh@gmail.com>
2021-08-18 13:54:39 +0530

    nc

--- core/settings.py
+++ core/settings.py
@@ -169,4 +169,4 @@ CHANNEL_LAYERS = {
 
 Temp=json.loads(os.environ.get('GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE_CONTENTS'))
 
-GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = json.dumps(Temp)
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = os.path.abspath('coral-sum-321710-a4637f09d154.json')
