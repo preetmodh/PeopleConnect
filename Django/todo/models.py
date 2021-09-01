ea4bfdd56f849ffc538fbcf5cf97525fdb513354
preetmodh <preetmodh@gmail.com>
2021-09-01 09:28:10 +0530

    nc

--- Django/todo/models.py
+++ Django/todo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-
+import django
 
 
 class TodoData(models.Model):
@@ -9,5 +9,5 @@ class TodoData(models.Model):
     title=models.CharField(max_length=300)
     desc=models.TextField()
     created=models.DateTimeField(auto_now_add=True )
-    due_date = models.DateTimeField(default=datetime.now())
+    due_date = models.DateTimeField(default=django.utils.timezone.now)
     complete = models.BooleanField(default=False)
