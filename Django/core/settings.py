14926fc903cae206ebeef540483bf9313a90957f
Preet Modh <preetmodh@gmail.com>
2022-12-25 13:50:11 +0530

    nc

--- Django/core/settings.py
+++ Django/core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-&qwqu)ezx88g*tj#)sr(w#z^usi+@6mwtbb@7o$!qg90dwdvtg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1','7c57-2401-4900-1953-23b-9d8b-86d6-876f-4b44.in.ngrok.io']
+ALLOWED_HOSTS = ['people-connect.vercel.app','127.0.0.1','7c57-2401-4900-1953-23b-9d8b-86d6-876f-4b44.in.ngrok.io']
 
 
 # Application definition
@@ -141,7 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS=True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://people-connect.vercel.app"
 ]
 
 
