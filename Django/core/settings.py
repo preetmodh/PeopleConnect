b162bd46f71c82cf9666cb5df2599500a30d0eb3
Preet Modh <preetmodh@gmail.com>
2023-08-20 14:10:58 +0530

    nc

--- Django/core/settings.py
+++ Django/core/settings.py
@@ -80,23 +80,13 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'database',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 db_from_env =dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
