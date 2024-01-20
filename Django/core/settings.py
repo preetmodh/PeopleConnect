086dbfb3e7b76ac3e3f07bb7fe7385f4750bbca2
Preet Modh <preetmodh@gmail.com>
2024-01-20 22:25:20 +0530

    removing psql

--- Django/core/settings.py
+++ Django/core/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL)
-DATABASES['default'].update(db_from_env)
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# db_from_env = dj_database_url.config(default=DATABASE_URL)
+# DATABASES['default'].update(db_from_env)
 
 
 
